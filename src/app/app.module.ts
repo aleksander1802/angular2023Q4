4141897ad2a1e9f4e770efc713bbd90d2bcf27b8
Alex2 <aleksander1802@gmail.com>
2023-11-04 02:07:48 +0300

    refactor: removed mainlayout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [AppComponent, MainLayoutComponent],
+    declarations: [AppComponent],
     providers: [],
     bootstrap: [AppComponent],
     imports: [BrowserModule, AppRoutingModule, CoreModule],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { CustomButtonComponent } from './components/custom-button/custom-button.component';
 import { MaterialModule } from '../material/material.module';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
 @NgModule({
-    declarations: [CustomButtonComponent],
-    imports: [CommonModule, MaterialModule],
+    declarations: [CustomButtonComponent, MainLayoutComponent],
+    imports: [CommonModule, MaterialModule, RouterModule],
     exports: [CustomButtonComponent, MaterialModule],
 })
 export class SharedModule {}
