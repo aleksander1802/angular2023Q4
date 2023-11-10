d1cf602581c7bd7602d7429dccd80f36e5a16610
Alex2 <aleksander1802@gmail.com>
2023-11-10 15:22:33 +0300

    fix: fixed auth guards according to isloggedin observable

--- src/app/auth/guards/login.guard.ts
+++ src/app/auth/guards/login.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Router, CanActivateFn } from '@angular/router';
+import { map } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({ providedIn: 'root' })
@@ -7,12 +8,15 @@ export class LoginPermissionsService {
     constructor(private router: Router, private authService: AuthService) {}
 
     canActivate() {
-        if (!this.authService.isLoggedIn()) {
-            return true;
-        }
-
-        this.router.navigate(['/youtube']);
-        return false;
+        return this.authService.isLoggedIn$.pipe(
+            map((isLoggedIn) => {
+                if (!isLoggedIn) {
+                    return true;
+                }
+                this.router.navigate(['/youtube']);
+                return false;
+            })
+        );
     }
 }
 
