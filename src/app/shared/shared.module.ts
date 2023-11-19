f9a23d5ae045e81209f6ca38880fca9885215722
Alex2 <aleksander1802@gmail.com>
2023-11-19 19:01:12 +0300

    refactor: pipes and directive have been moved to the shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,29 @@ import { RouterModule } from '@angular/router';
 import { CustomButtonComponent } from './components/custom-button/custom-button.component';
 import { MaterialModule } from '../material/material.module';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { MetricsComponent } from '../youtube/components/metrics/metrics.component';
+import { ColorByDateDirective } from './directives/colorByDate/color-by-date.directive';
+import { FilteringPipe } from './pipes/filtering/filtering.pipe';
+import { SortingPipe } from './pipes/sorting/sorting.pipe';
 
 @NgModule({
-    declarations: [CustomButtonComponent, MainLayoutComponent],
+    declarations: [
+        CustomButtonComponent,
+        MainLayoutComponent,
+        MetricsComponent,
+        SortingPipe,
+        FilteringPipe,
+        ColorByDateDirective,
+    ],
     imports: [CommonModule, MaterialModule, RouterModule],
-    exports: [CustomButtonComponent, MaterialModule, CommonModule],
+    exports: [
+        CommonModule,
+        MaterialModule,
+        CustomButtonComponent,
+        MetricsComponent,
+        SortingPipe,
+        FilteringPipe,
+        ColorByDateDirective,
+    ],
 })
 export class SharedModule {}
--- src/app/youtube/youtube.module.ts
+++ src/app/youtube/youtube.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { SearchResultsComponent } from './components/search/search-results/search-results.component';
 import { SearchItemComponent } from './components/search/search-item/search-item.component';
-import { SortingPipe } from './pipes/sorting/sorting.pipe';
-import { FilteringPipe } from './pipes/filtering/filtering.pipe';
-import { ColorByDateDirective } from './directives/colorByDate/color-by-date.directive';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SharedModule } from '../shared/shared.module';
-import { MetricsComponent } from './components/metrics/metrics.component';
 import { DetailedInformationPageComponent } from './pages/detailed-information-page/detailed-information-page.component';
 import { YoutubeRoutingModule } from './youtube-routing.module';
 
@@ -14,11 +10,7 @@ import { YoutubeRoutingModule } from './youtube-routing.module';
     declarations: [
         SearchResultsComponent,
         SearchItemComponent,
-        SortingPipe,
-        FilteringPipe,
-        ColorByDateDirective,
         HomePageComponent,
-        MetricsComponent,
         DetailedInformationPageComponent,
     ],
 
